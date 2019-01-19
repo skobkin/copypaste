95c652e4d63134175f19f6140d681d4e6e67b359
Alexey Skobkin <skobkin-ru@ya.ru>
2019-01-20 02:42:01 +0300

    Missing bundle fix in kernel.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,7 +25,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
         }
 
         return $bundles;
