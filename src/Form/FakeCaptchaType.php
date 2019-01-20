11b4903cf1b6466bc7ed81b8741dcd1881c2b8b7
Alexey Skobkin <skobkin-ru@ya.ru>
2019-01-20 07:13:52 +0300

    Redundant import removed.

--- src/Form/FakeCaptchaType.php
+++ src/Form/FakeCaptchaType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FakeCaptchaType extends AbstractType
